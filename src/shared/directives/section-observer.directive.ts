a7d1dd34d0d1cba3672149cad67a16f8b062d321
cmelo <m.celin@gmail.com>
2026-02-22 19:27:14 +0100

    ssr fix for navigation

--- src/shared/directives/section-observer.directive.ts
+++ src/shared/directives/section-observer.directive.ts
@@ -1,8 +1,10 @@
+import { isPlatformBrowser } from '@angular/common';
 import {
 	DestroyRef,
 	Directive,
 	ElementRef,
 	OnInit,
+	PLATFORM_ID,
 	inject,
 	input,
 } from '@angular/core';
@@ -16,10 +18,13 @@ export class SectionObserverDirective implements OnInit {
 	private _el = inject<ElementRef<HTMLElement>>(ElementRef);
 	private _activeSection = inject(MenuService);
 	private _destroyRef = inject(DestroyRef);
+	private _platformId = inject(PLATFORM_ID);
 
 	public cSectionObserver = input.required<MenuId>();
 
 	public ngOnInit(): void {
+		if (!isPlatformBrowser(this._platformId)) return;
+
 		const observer = new IntersectionObserver(
 			(entries) => {
 				const visible = entries.find((e) => e.isIntersecting);
